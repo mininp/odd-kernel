13e59ede1bf1fb30f6f8df70cd41c880873c7f75
not ervin <notminin@pm.me>
2019-04-03 23:00:41 +0100

    add console buffer

--- src/console.js
+++ src/console.js
@@ -1,6 +1,8 @@
 const width = document.body.clientWidth;
 const height = document.body.clientHeight;
 
+let buffer = "Testing Testing 123\nHello, World!\n\n[x@y ~]$ _"
+
 let c = document.createElement("canvas");
 document.body.appendChild(c);
 c.width = width;
@@ -8,11 +10,21 @@ c.height = height;
 
 let ctx = c.getContext("2d");
 ctx.lineWidth = 2;
-ctx.font = "1em Arial";
+ctx.font = "1em monospace";
 
 (function render() {
 	window.requestAnimationFrame(render);
 
 	ctx.fillStyle = "#161621";
 	ctx.fillRect(0, 0, width, height);
+
+	ctx.fillStyle = "#ddd";
+
+	buffer.split("\n").forEach((string, i) => {
+		ctx.fillText(string, 0, em(i + 1));
+	});
 })();
+
+function em(x = 1) {
+	return x * parseFloat(getComputedStyle(document.body).fontSize);
+}
