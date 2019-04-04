84c54de238a89080c921e1981707d65537006fc9
not ervin <notminin@pm.me>
2019-04-04 21:43:59 +0100

    add writing in console

--- src/console.js
+++ src/console.js
@@ -2,7 +2,23 @@ const width = document.body.clientWidth;
 const height = document.body.clientHeight;
 const maxchar = document.body.clientWidth / em();
 
-let buffer = "Testing Testing 123\nHello, World!\n\n[x@y ~]$ _";
+let inputBuffer = "";
+let buffer = "ODD Kernel Console\n";
+let ps1 = "$ ";
+
+document.addEventListener("keydown", (e) => {
+	if (e.code == "Backspace") {
+		if (inputBuffer.length > 0) {
+			inputBuffer = inputBuffer.substring(0, inputBuffer.length - 1);
+		}
+	} else {
+		if (e.key.length == 1) {
+			inputBuffer += e.key;
+		}
+	}
+
+	blink = true;
+});
 
 let c = document.createElement("canvas");
 document.body.appendChild(c);
@@ -13,7 +29,15 @@ let ctx = c.getContext("2d");
 ctx.lineWidth = 2;
 ctx.font = "1em monospace";
 
+let frame = 0;
+let blink = false;
+
 (function render() {
+	frame++;
+
+	if (frame % 60 == 0)
+		blink = !blink;
+
 	window.requestAnimationFrame(render);
 
 	ctx.fillStyle = "#161621";
@@ -21,9 +45,23 @@ ctx.font = "1em monospace";
 
 	ctx.fillStyle = "#ddd";
 
-	buffer.split("\n").forEach((string, i) => {
-		ctx.fillText(string, 0, em(i + 1));
-	});
+	{ // draw text
+		let index;
+	
+		// buffer
+		buffer.split("\n").forEach((string, i) => {
+			index = i;
+			ctx.fillText(string, 0, em(i + 1));
+		});
+	
+		// prompt
+		let p = ps1 + inputBuffer;
+	
+		if (blink)
+			p += "|";
+	
+		ctx.fillText(p, 0, em(index + 2));
+	}
 })();
 
 function em(x = 1) {
